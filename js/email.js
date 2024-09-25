fbd13a54b7343103ef8acb5c39176686e88bd282
Carlos Salcán <cdsalcan131@gmail.com>
2024-09-24 22:22:48 -0500

    Implementación de envio de correos electrónicos
    - Se corrigió la configuración del formulario para enviar correos a través de EmailJS.
    - Se mejoró la gestión de parámetros del script email..

--- js/email.js
+++ js/email.js
@@ -1,10 +1,30 @@
-function sendMail() {
-    let parms = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value,
-    }
+(function () {
+    //Public key
+    emailjs.init("OVb7N-XSAjgadhv8x");
+})();
 
-    emailjs.send("", "template_s2puvti", parms).then(alert("Correo Enviado Exitosamente!!"))
-}
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    var templateParams = {
+        from_name: name,
+        from_email: email,
+        message: message,
+        to_name: " Email Js demo"
+    };
+    console.log(templateParams)
+
+    //service key, template id
+    emailjs.send("service_tpe695j", "template_sz4ls1z", templateParams)
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Message sent successfully!');
+        }, function (error) {
+            console.log('FAILED...', error);
+            alert('Message failed to send.');
+        });
+
+});
